d66e4e09cdc734d54291cfccf5cdb095e248e526
joersaac <00159619@uca.edu.sv>
2023-06-08 16:35:36 -0600

    :package: Added MUI packages

---
+++ src/Style/Theming.js
@@ -0,0 +1,23 @@
+import { createTheme } from '@mui/material/styles';
+
+let theme = createTheme({
+    palette: {
+        primary: {
+        main: '#24406C',
+        },
+        secondary: {
+        main: '#E0E0E0',
+        },
+        background: {
+            main: '#edf2ff',
+        },
+        secondary_button: {
+            main: '#a10e25',
+        },
+        secondary: {
+            main: '#edf2ff',
+        },
+    },
+});
+
+export {theme};
