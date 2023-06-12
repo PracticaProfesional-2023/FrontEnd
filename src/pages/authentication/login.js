48f8764693fa0536721ec70e8735e638783f42b4
joersaac <00159619@uca.edu.sv>
2023-06-11 23:33:07 -0600

    :construction: Working on login email input

--- src/pages/authentication/login.js
+++ src/pages/authentication/login.js
@@ -2,7 +2,8 @@ import * as React from 'react';
 import { Card, Box, ThemeProvider, TextField, Button } from '@mui/material';
 import { theme } from '../../Style/Theming';
 import Logo from '../../assets/Logo-bckBlue.png';
-import { Link } from 'react-router-dom';/*
+//import { Link } from 'react-router-dom';
+/*
 import {
     useQuery,
     useMutation,
@@ -11,25 +12,26 @@ import {
     QueryClientProvider,
 } from 'react-query';*/
 import axios from "axios";
-import { useForm } from "react-hook-form";
+import { Controller, useForm } from "react-hook-form";
 
 //const queryClient = new QueryClient();
 axios.defaults.baseURL = 'https://helpful-woolens-elk.cyclic.app';
 
-const sendOtp = async () => {
-    axios.post('/auth/send-candidate-otp', 
-        {
-            "email": "rodrigo.mejia20162@gmail.com"
-        })
-    .then(function (response) {
-        console.log(response);
-    })
-    .catch(function (error) {
-        console.log(error);
-    });
-};
 
 const Login = () => {
+    const { handleSubmit, control } = useForm();
+
+    const sendOtp = async (data) => {
+        axios.post('/auth/send-candidate-otp', 
+            data)
+        .then(function (response) {
+            console.log(response);
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
+    };
+
     return (
         //<QueryClientProvider client={queryClient}>
             <ThemeProvider theme={theme}>
@@ -53,18 +55,24 @@ const Login = () => {
                         style={{background: "#e0e0e0"}}
                         >
                         <img src = {Logo} alt='logo' width="30%"/>
-                        <TextField id="Email" label="Email" variant="outlined" 
-                            inputProps={{style: {fontSize: 12}}} InputLabelProps={{style: {fontSize: 18}}} 
-                            sx={{width: "50%", height: "25%"}} />
-                        <Link to="/hirejobs/login/confirmation_code" style={{ color: '#FFFFFF' }} >
-                            <Button type='submit' variant="contained" color="primary"
-                                onClick={() => {
-                                    sendOtp();}}>Login</Button>
-                        </Link>
+                        <Controller
+                        name={"email"}
+                        control={control}
+                        defaultValue={""}
+                        render={({ field: { onChange, value } }) => (
+                            <TextField onChange={onChange} value={value} label={"email"}
+                                variant="outlined" 
+                                inputProps={{style: {fontSize: 12}}} InputLabelProps={{style: {fontSize: 18}}} 
+                                sx={{width: "50%", height: "25%"}} />
+                        )} />
+                        <Button variant="contained" color="primary"
+                            onClick={handleSubmit(sendOtp)}
+                            >Login</Button>                     
                     </Card>
                 </Box>
             </ThemeProvider>
         //</QueryClientProvider>
+        
     );
 }
 
